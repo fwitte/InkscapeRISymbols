2ecd1a215a8557dc1add7007bb089ce8a70d42a3
jfreissmann <j.freissmann@web.de>
2020-05-06 11:46:14 +0200

    Add drawing functionality for drums

--- drawRINodes.py
+++ drawRINodes.py
@@ -5,12 +5,14 @@ import inkscapeMadeEasy_Draw as inkDraw
 
 from drawRIComponents import component
 
+from numpy import sqrt, sin, cos, pi
 
-class piping(component):
+
+class node(component):
     # ---------------------------------------------
-    def drawPipe(self, parent, position=[0, 0], label='Pipe',
-                 direction='right', angleDeg=0):
-        """Draw a generic pipe.
+    def drawDrum(self, parent, position=[0, 0], label='Drum',
+                 direction='right', angleDeg=0, nodeType='drum'):
+        """Draw a drum.
 
         parent: parent object
         position: position [x,y]
@@ -18,15 +20,90 @@ class piping(component):
         label: label of the object (it can be repeated)
         angleDeg: rotation angle in degrees counter-clockwise (default 0)
         """
+        group = self.createGroup(parent, label)
+        elem = self.createGroup(group)
 
-    # ---------------------------------------------
-    def drawHose(self, parent, position=[0, 0], label='Hose',
-                 direction='right', angleDeg=0):
-        """Draw a hose.
+        # define radius of main circle
+        self.radius = self.componentExtent / 2
 
-        parent: parent object
-        position: position [x,y]
+        # draw main circle
+        inkDraw.circle.centerRadius(
+            elem, [self.radius, 0], self.radius,
+            offset=position, label='circle', lineStyle=self.lineStyle
+        )
 
-        label: label of the object (it can be repeated)
-        angleDeg: rotation angle in degrees counter-clockwise (default 0)
-        """
+        # draw horizontal diameter
+        inkDraw.line.relCoords(
+            elem, [[self.componentExtent, 0]], position,
+            lineStyle=self.lineStyle)
+
+        # draw upper vertical connector
+        inkDraw.line.relCoords(
+            elem, [[0, - self.connectorLength]],
+            [position[0] + self.radius, position[1] - self.radius],
+            lineStyle=self.lineStyle)
+
+        # draw lower vertical connector
+        inkDraw.line.relCoords(
+            elem, [[0, self.connectorLength]],
+            [position[0] + self.radius, position[1] + self.radius],
+            lineStyle=self.lineStyle)
+
+        # change direction to the users choice
+        if direction == 'right':
+            # draw upper diagonal connector
+            inkDraw.line.relCoords(
+                elem, [[sqrt(2)/2 * self.connectorLength,
+                        - sqrt(2)/2 * self.connectorLength]],
+                [position[0] + self.radius * (1 + cos(pi/4)),
+                 position[1] - self.radius * sin(pi/4)],
+                lineStyle=self.lineStyle)
+
+            # draw lower diagonal connector
+            inkDraw.line.relCoords(
+                elem, [[sqrt(2)/2 * self.connectorLength,
+                        sqrt(2)/2 * self.connectorLength]],
+                [position[0] + self.radius * (1 + cos(pi/4)),
+                 position[1] + self.radius * sin(pi/4)],
+                lineStyle=self.lineStyle)
+
+            # change label position depending on chosen direction
+            text_x_offset = (position[0]
+                             - self.connectorLength
+                             - self.textOffset * self.scale)
+
+        elif direction == 'left':
+            # draw upper diagonal connector
+            inkDraw.line.relCoords(
+                elem, [[- sqrt(2)/2 * self.connectorLength,
+                        - sqrt(2)/2 * self.connectorLength]],
+                [position[0] + self.radius * (1 - cos(pi/4)),
+                 position[1] - self.radius * sin(pi/4)],
+                lineStyle=self.lineStyle)
+
+            # draw lower diagonal connector
+            inkDraw.line.relCoords(
+                elem, [[- sqrt(2)/2 * self.connectorLength,
+                        sqrt(2)/2 * self.connectorLength]],
+                [position[0] + self.radius * (1 - cos(pi/4)),
+                 position[1] + self.radius * sin(pi/4)],
+                lineStyle=self.lineStyle)
+
+            # change label position depending on chosen direction
+            text_x_offset = (position[0]
+                             + self.componentExtent
+                             + self.connectorLength
+                             + self.textOffset * self.scale)
+
+        # change rotation to user defined angle
+        if angleDeg != 0:
+            self.rotateElement(group, position, angleDeg)
+
+        pos_text = [text_x_offset, position[1]]
+
+        inkDraw.text.write(
+            self, label, pos_text, group, fontSize=self.fontSize,
+            justification='center', textStyle=self.textStyle
+        )
+
+        return group
