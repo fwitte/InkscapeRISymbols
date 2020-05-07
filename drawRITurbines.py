48f763a18ebf34e5f72bd47225c3d6f631c32dd7
maltefritz <malte.fritz@web.de>
2020-05-07 15:03:30 +0200

    Add mirroring and extraction option

--- drawRITurbines.py
+++ drawRITurbines.py
@@ -4,7 +4,7 @@ import inkscapeMadeEasy_Base as inkBase
 import inkscapeMadeEasy_Draw as inkDraw
 
 from drawRIComponents import component
-
+import numpy as np
 
 class turbine(component):
     # ---------------------------------------------
@@ -30,7 +30,7 @@ class turbine(component):
 
         # diagonally top right
         inkDraw.line.relCoords(
-            elem, [[self.componentExtent * 1.5, -self.componentExtent / 2]],
+            elem, [[self.componentExtent * 1.5, - self.componentExtent / 2]],
             position,
             lineStyle=self.lineStyle
         )
@@ -51,58 +51,100 @@ class turbine(component):
             lineStyle=self.lineStyle
         )
 
+        # label position
+        pos_text = [
+            position[0] + self.componentExtent * 1.5 + (3 * self.scale),
+            position[1] + self.componentExtent / (4 * self.scale) -
+            self.textOffset * self.scale]
+
+        inkDraw.text.write(
+            self, label, pos_text, group, fontSize=self.fontSize,
+            justification='left', textStyle=self.textStyle
+        )
+
         if connection == 'crossed':
-            # inlet
+            if mirroring == 'mirOff':
+                # inlet crossed - no mirror
+                inkDraw.line.relCoords(
+                    elem, [[0, - self.connectorLength]],
+                    position,
+                    lineStyle=self.lineStyle
+                )
+
+                # outlet crossed - no mirror
+                inkDraw.line.relCoords(
+                    elem, [[0, self.connectorLength]],
+                    [position[0] + self.componentExtent * 1.5,
+                    position[1] + self.componentExtent * 2],
+                    lineStyle=self.lineStyle
+                )
+
+            if mirroring == 'mirOn':
+                # inlet crossed - mirror
+                inkDraw.line.relCoords(
+                    elem, [[0, + self.connectorLength]],
+                    [position[0],
+                    position[1] + self.componentExtent * 1.5],
+                    lineStyle=self.lineStyle
+                )
+
+                # outlet crossed - mirror
+                inkDraw.line.relCoords(
+                    elem, [[0, - self.connectorLength]],
+                    [position[0] + self.componentExtent * 1.5,
+                    position[1] - self.componentExtent / 2],
+                    lineStyle=self.lineStyle
+                )
+
+        if connection == 'equal':
+            if mirroring == 'mirOff':
+                # inlet equal - no mirror
+                inkDraw.line.relCoords(
+                    elem, [[0, self.connectorLength]],
+                    [position[0],
+                    position[1] + self.componentExtent * 1.5],
+                    lineStyle=self.lineStyle
+                )
+
+                # outlet equal - no mirror
+                inkDraw.line.relCoords(
+                    elem, [[0, self.connectorLength]],
+                    [position[0] + self.componentExtent * 1.5,
+                    position[1] + self.componentExtent * 2],
+                    lineStyle=self.lineStyle
+                )
+
+            if mirroring == 'mirOn':
+                # inlet equal - mirror
+                inkDraw.line.relCoords(
+                    elem, [[0, - self.connectorLength]],
+                    position,
+                    lineStyle=self.lineStyle
+                )
+
+                # outlet equal - mirror
+                inkDraw.line.relCoords(
+                    elem, [[0, - self.connectorLength]],
+                    [position[0] + self.componentExtent * 1.5,
+                    position[1] - self.componentExtent / 2],
+                    lineStyle=self.lineStyle
+                )
+
+        if extraction == 'extrTop':
             inkDraw.line.relCoords(
-                elem, [[0, -self.connectorLength]],
-                position,
+                elem, [[0, - self.connectorLength]],
+                [position[0] + self.connectorLength * 1.5,
+                position[1] - self.connectorLength / 2],
                 lineStyle=self.lineStyle
             )
 
-            # outlet
+        if extraction == 'extrDown':
             inkDraw.line.relCoords(
-                elem, [[0, self.connectorLength]],
-                [position[0] + self.componentExtent * 1.5,
-                position[1] + self.componentExtent * 2],
+                elem, [[0, + self.connectorLength]],
+                [position[0] + self.connectorLength * 1.5,
+                position[1] + self.componentExtent * 1.5
+                 + self.connectorLength / 2],
                 lineStyle=self.lineStyle
             )
-
-            pos_text = [
-                position[0] + self.componentExtent * 1.5 + (3 * self.scale),
-                position[1] + self.componentExtent / (4 * self.scale) -
-                self.textOffset * self.scale]
-
-            inkDraw.text.write(
-                self, label, pos_text, group, fontSize=self.fontSize,
-                justification='left', textStyle=self.textStyle
-            )
-
-        elif connection == 'equal':
-            # inlet
-            inkDraw.line.relCoords(
-                elem, [[0, self.connectorLength]],
-                [position[0],
-                position[1] + self.componentExtent * 1.5],
-                lineStyle=self.lineStyle
-            )
-
-            # outlet
-            inkDraw.line.relCoords(
-                elem, [[0, self.connectorLength]],
-                [position[0] + self.componentExtent * 1.5,
-                position[1] + self.componentExtent * 2],
-                lineStyle=self.lineStyle
-            )
-
-            pos_text = [
-                position[0],
-                position[1] - self.componentExtent / (4 * self.scale) -
-                self.textOffset * self.scale]
-
-            inkDraw.text.write(
-                self, label, pos_text, group, fontSize=self.fontSize,
-                justification='left', textStyle=self.textStyle
-            )
-
 
         return group
